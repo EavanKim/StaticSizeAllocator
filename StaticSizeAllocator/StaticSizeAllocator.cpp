66ffd39382c27bca20543354a9bbc95888164ef2
eavankim <kjtkjt21@gmail.com>
2023-01-19 13:43:39 +0900

    [Setup] initialize Commit
    Solution Setup

---
+++ StaticSizeAllocator/StaticSizeAllocator.cpp
@@ -0,0 +1,21 @@
+﻿// StaticSizeAllocator.cpp : DLL을 위해 내보낸 함수를 정의합니다.
+//
+
+#include "framework.h"
+#include "StaticSizeAllocator.h"
+
+
+// 내보낸 변수의 예제입니다.
+STATICSIZEALLOCATOR_API int nStaticSizeAllocator=0;
+
+// 내보낸 함수의 예제입니다.
+STATICSIZEALLOCATOR_API int fnStaticSizeAllocator(void)
+{
+    return 0;
+}
+
+// 내보낸 클래스의 생성자입니다.
+CStaticSizeAllocator::CStaticSizeAllocator()
+{
+    return;
+}
